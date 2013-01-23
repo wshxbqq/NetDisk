c704985a0694ca303ee619910b0d4ea4c1a88f9a
wshxbqq <wshxbqq@gmail.com>
2013-01-24 03:10:08 +0800

    add node

---
+++ NodeJS/websocket/WebsocketServer.js
@@ -0,0 +1,20 @@
+//npm install socket.io
+
+var io = require('socket.io').listen(8888);
+var userList=[];
+io.sockets.on('connection', function (socket) {
+  userList.push(socket);
+  socket.send ( 'Now connected!' );
+  console.log("loged");
+
+  socket.on('message', function (event) { 
+    for (var i = 0; i < userList.length; i++) {
+     var innerSocket= userList[i];
+     innerSocket.send(event+"_from server");
+    };
+  });
+  socket.on('disconnect', function (event) {
+   console.log(event);
+   });
+   console.log("connectioned");
+});
