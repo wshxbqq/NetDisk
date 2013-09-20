56eaf7fbced94ba76952e57b7ab07aa34795b624
OpenShift guest <wshxbqq@gmail.com>
2013-09-20 15:46:17 -0400

    123

--- javascript/phantomjs/fastDemo.js
+++ javascript/phantomjs/fastDemo.js
@@ -1,12 +1,12 @@
 console.log('Loading a web page');
 var page = require('webpage').create();
-var url = 'http://www.youku.com/';
+var url = 'http://www.google.com/';
 page.open(url, function (status) {
     //Page is loaded!
     //phantom.exit();
 });
 
 window.setTimeout(function () {
-    page.render('D:/t.jpg');
+    page.render('a.png');
     phantom.exit();
 }, 6000);
